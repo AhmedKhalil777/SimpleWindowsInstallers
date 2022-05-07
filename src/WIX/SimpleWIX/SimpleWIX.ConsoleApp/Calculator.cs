df3e25edeb1ad193f2ef6cac23cf058485a695bc
Ahmed Khalil <Progeng_ahmed_khalil@outlook.com>
2022-05-07 23:34:03 +0200

    WIP

---
+++ src/WIX/SimpleWIX/SimpleWIX.ConsoleApp/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleWIX.ConsoleApp
+{
+    public static class CalculatorProvider
+    {
+        public static Func<T, T, T> BuildOperation<T>(string op)
+            where T : struct
+        {
+            return op switch
+            {
+                "+" => new Func<T,T,T>((T first, T second) => (T)((dynamic)first+ (dynamic)second)),
+                "-" => new Func<T, T, T>((T first, T second) => (T)((dynamic)first - (dynamic)second)),
+                "*" => new Func<T, T, T>((T first, T second) => (T)((dynamic)first * (dynamic)second)),
+                "/" => new Func<T, T, T>((T first, T second) => (T)((dynamic)first / (dynamic)second)),
+                "%" => new Func<T, T, T>((T first, T second) => (T)((dynamic)first % (dynamic)second)),
+                _ => throw new ArgumentException()
+            };
+        }
+    }
+}
---
+++ src/WIX/SimpleWIX/SimpleWIX.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SimpleWIX.ConsoleApp;
+
+while (true)
+{
+    Console.Write("Please Enter Operation (+, - , * , /) : ");
+    var op = CalculatorProvider.BuildOperation<double>(op: Console.ReadLine());
+    Console.Write("Enter the first num : ");
+    var num1 = double.Parse(Console.ReadLine());
+    Console.Write("Enter the second num : ");
+    var num2 = double.Parse(Console.ReadLine());
+    Console.WriteLine(op(num1,num2));
+}
