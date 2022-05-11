6abf638ac7d634b8253bccd05b17a5cf262d6d68
Ahmed Khalil <Progeng_ahmed_khalil@outlook.com>
2022-05-11 02:16:11 +0200

    Start the Worker and Wpf installers

---
+++ src/WIX/SimpleWIX/SimpleWIX.WorkerTCPService/Program.cs
@@ -0,0 +1,10 @@
+using SimpleWIX.WorkerTCPService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
